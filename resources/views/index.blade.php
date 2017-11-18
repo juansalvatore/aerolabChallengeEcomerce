11a70a2a85ce33e9b7b77344e436912f623f577c
Juan Salvatore <juansalvatore@live.com.ar>
2017-11-18 17:18:33 -0300

    Finish header bar with dynamic point ammount and user name

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,14 @@
 @section('header')
   <div class="header">
     <img src="aerolab-logo.svg" alt="aerolab">
+    <div class="userInfo">
+      <span class="userName">{{ $user['name'] }}</span>
+      <div class="pointAmount">
+        <span>{{ $user['points'] }}</span><img class="goldIconUserInfo" src="GoldIcon.svg">
+      </div>
+    </div>
   </div>
+
 @endsection
 
 @section('products')
--- resources/views/templates/landing.blade.php
+++ resources/views/templates/landing.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>aerolab</title>
-    <link rel="shortcut icon" type="image/png" href="aerolabFavicon.png"/>
+    <link rel="shortcut icon" type="image/png" href="aerolabFavicon.png" sizes="32x32"/>
+
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
     <link rel="stylesheet" href="/css/product/product-container-style.min.css">
   </head>
